263df2ec8cc6e12cfbc549ad3c8ae45140408c15
Mamatha_macharla <macharlamamathareddy@gmail.com>
2023-03-02 11:22:29 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {Route , BrowserRouter, Routes} from 'react-router-dom'
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import BookingCar from './Pages/BookingCar';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Routes>
+      <Route path='' exact element={<Home/>}/>
+      <Route path='/login' exact element={<Login/>}/>
+      <Route path='/register' exact element={<Register/>}/>
+      <Route path='/bookingcar' exact element={<BookingCar/>}/>
+      </Routes>
+      </BrowserRouter>
+
+      
+    </div>
+  );
+}
+
+export default App;
